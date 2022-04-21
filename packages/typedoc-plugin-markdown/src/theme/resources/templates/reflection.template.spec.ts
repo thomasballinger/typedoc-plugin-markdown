8f19de8e33007b05cd28800464a059a6bd08b3fa
tgreyuk <tgreyuk@gmail.com>
2022-04-21 21:54:34 +0100

    x

--- packages/typedoc-plugin-markdown/src/theme/resources/templates/reflection.template.spec.ts
+++ packages/typedoc-plugin-markdown/src/theme/resources/templates/reflection.template.spec.ts
@@ -1,72 +1,46 @@
-import { ProjectReflection } from 'typedoc';
-import { TestApp } from '../../../../test/test-app';
-import { MarkdownThemeRenderContext } from '../../theme.context';
+import { context, project } from '../../../../test/test-utils';
 import { reflectionTemplate } from './reflection.template';
 
 describe(`Reflection Template:`, () => {
-  let testApp: TestApp;
-  let project: ProjectReflection;
-  let context: MarkdownThemeRenderContext;
-
-  beforeEach(async () => {
-    testApp = new TestApp(['index.ts']);
-
-    testApp.bootstrap();
-
-    project = testApp.app.convert() as ProjectReflection;
-
-    context = {
-      breadcrumbsPartial: (props: any) => '[breadcrumbsPartial]',
-      commentPartial: (props: any) => '[commentPartial]',
-      groupsPartial: (props: any) => '[groupsPartial]',
-      indexSignatureTitlePartial: (props: any) =>
-        '[indexSignatureTitlePartial]',
-      reflectionPathPartial: (props: any) => '[reflectionPathPartial]',
-      reflectionTitlePartial: (props: any) => '[reflectionTitlePartial]',
-      signaturePartial: (props: any) => '[reflectionTitlePartial]',
-      tocPartial: (props: any) => '[tocPartial]',
-      typePartial: (props: any) => '[typePartial]',
-      options: {},
-    } as any;
-  });
+  beforeEach(async () => {});
 
   test(`should compile project reflection`, () => {
     const props = {
-      project: project,
-      model: project,
+      project: project(),
+      model: project(),
     } as any;
-    expect(reflectionTemplate(context, props)).toMatchSnapshot();
+    expect(reflectionTemplate(context(), props)).toMatchSnapshot();
   });
 
   test(`should compile callable reflection`, () => {
     const props = {
-      project: project,
-      model: project.findReflectionByName('CallableReflection'),
+      project: project(),
+      model: project().findReflectionByName('CallableReflection'),
     } as any;
-    expect(reflectionTemplate(context, props)).toMatchSnapshot();
+    expect(reflectionTemplate(context(), props)).toMatchSnapshot();
   });
 
   test(`should compile indexable reflection`, () => {
     const props = {
-      project: project,
-      model: project.findReflectionByName('IndexableReflection'),
+      project: project(),
+      model: project().findReflectionByName('IndexableReflection'),
     } as any;
-    expect(reflectionTemplate(context, props)).toMatchSnapshot();
+    expect(reflectionTemplate(context(), props)).toMatchSnapshot();
   });
 
-  test(`should compile implemented class`, () => {
+  test(`should compile implemented class reflection`, () => {
     const props = {
-      project: project,
-      model: project.findReflectionByName('ImplementedClass'),
+      project: project(),
+      model: project().findReflectionByName('ImplementedClass'),
     } as any;
-    expect(reflectionTemplate(context, props)).toMatchSnapshot();
+    expect(reflectionTemplate(context(), props)).toMatchSnapshot();
   });
 
-  test(`should compile Enum`, () => {
+  test(`should compile enum reflection`, () => {
     const props = {
-      project: project,
-      model: project.findReflectionByName('EnumReflection'),
+      project: project(),
+      model: project().findReflectionByName('EnumReflection'),
     } as any;
-    expect(reflectionTemplate(context, props)).toMatchSnapshot();
+    expect(reflectionTemplate(context(), props)).toMatchSnapshot();
   });
 });
